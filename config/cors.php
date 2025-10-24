5e94ef26640aebde1c54dc5d243c2cb1f0b7300f
kikarin <akunamazon4811@gmail.com>
2025-10-24 16:24:25 +0700

    update cors

--- config/cors.php
+++ config/cors.php
@@ -1,13 +1,25 @@
 <?php
 
 return [
-    'paths'                    => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
-    'allowed_methods'          => ['*'],
-    'allowed_origins'          => ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://localhost:3001',
+        'http://localhost:3002',
+        'https://dispora.vercel.app',
+        'https://dispora-mobile.summitbreak.com',
+    ],
+
     'allowed_origins_patterns' => [],
-    'allowed_headers'          => ['*'],
-    'exposed_headers'          => ['*'],
-    'max_age'                  => 0,
-    'supports_credentials'     => true,
-    'allowed_credentials'      => true,
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => ['*'],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
 ];
